c0a571c216007e1290083fff43a93eace0b042a1
XpLoDWilD <xplodgui@gmail.com>
2013-04-15 16:17:04 +0000

    find5: Add mpdecision and thermald
    Change-Id: I96b9bfd3ed00814c07637cb72deb64ed4b6f1a73

--- find5/find5-vendor-blobs.mk
+++ find5/find5-vendor-blobs.mk
@@ -82,7 +82,9 @@ PRODUCT_COPY_FILES += \
 	vendor/oppo/find5/proprietary/bin/qseecomd:system/bin/qseecomd \
 	vendor/oppo/find5/proprietary/bin/rmt_storage:system/bin/rmt_storage \
 	vendor/oppo/find5/proprietary/bin/sensors.qcom:system/bin/sensors.qcom \
-	vendor/oppo/find5/proprietary/bin/nv_tee:system/bin/nv_tee
+	vendor/oppo/find5/proprietary/bin/nv_tee:system/bin/nv_tee \
+	vendor/oppo/find5/proprietary/bin/mpdecision:system/bin/mpdecision \
+	vendor/oppo/find5/proprietary/bin/thermald:system/bin/thermald
 
 
 
